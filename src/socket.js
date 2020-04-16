e458aaa3df515eefa7eaa160656e493637d147bf
Marcio Fonseca <thefonseca@gmail.com>
2020-04-16 13:49:45 -0300

    Retornando a configuracão original de transports do socketio.

--- src/socket.js
+++ src/socket.js
@@ -2,17 +2,10 @@ import io from 'socket.io-client';
 
 export default function (socketUrl, customData, path) {
   const options = path ? { path } : {};
-  options['transports'] = ['websocket']
   const socket = io(socketUrl, options);
   socket.on('connect', () => {
     console.log(`connect:${socket.id}`);
     socket.customData = customData;
-  });
-
-  // on reconnection, reset the transports option, as the Websocket
-  // connection may have failed (caused by proxy, firewall, browser, ...)
-  socket.on('reconnect_attempt', () => {
-    socket.io.opts.transports = ['polling', 'websocket'];
   });
 
   socket.on('connect_error', (error) => {
